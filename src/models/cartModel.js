c99aeee0d2c5503e7daf05c5d1ca527b7729fb07
eliasescalante <eliasw.escalante@gmail.com>
2024-09-01 08:02:03 -0300

    arreglo en model/cart

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,14 +1,15 @@
-// Define un esquema para los carritos de compras
+	// Define un esquema para los carritos de compras
+	
+	const mongoose = require('mongoose');
+	
+	const cartSchema = new mongoose.Schema({
+            products: [{
+                product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+                quantity: { type: Number, required: true }
+            }]
+        });
+        
+	const Cart = mongoose.model('Cart', cartSchema);
+	
+	module.exports = Cart;
 
-const mongoose = require('mongoose');
-
-const cartSchema = new mongoose.Schema({
-    products: [{
-        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-        quantity: { type: Number, required: true }
-    }]
-});
-
-const Cart = mongoose.model('Cart', cartSchema);
-
-module.exports = Cart;
